3d6a08ace10e7eef45a5957454f5e2febd5cc9da
2chooman <2chooman@gmail.com>
2021-06-08 16:41:50 +0300

    Removing temp file

--- tempCodeRunnerFile.js
+++
@@ -1,5 +0,0 @@
-    for (var i = 0; i < b.length; i++) {
-        for (var j = 0; j < firstStepNumber.length; j++) {
-            if (b[i] % firstStepNumber[j] !== 0) firstStepNumber.splice(j, 1);
-        }
-    }
